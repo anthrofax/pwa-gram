d60f05fe2eefcf1eae3a3057b0669e9686a42900
anthrofax <afridhoikhsan@gmail.com>
2025-01-24 22:51:24 +0700

    41. Deferring the App Install Banner

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,14 @@
+let deferredPrompt;
+
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/sw.js").then(() => {
     console.log("Service Worker is registered");
   });
 }
+
+window.addEventListener("beforeinstallprompt", function (event) {
+  console.log("beforeinstallprompt fired");
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
