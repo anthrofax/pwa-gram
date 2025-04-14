f92bcd54534e1e078ec95879eb0bced4edd59a61
anthrofax <afridhoikhsan@gmail.com>
2025-04-14 20:31:36 +0700

    147. Requesting Permissions

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -11,14 +11,36 @@ if ('serviceWorker' in navigator) {
     .then(function () {
       console.log('Service worker registered!');
     })
-    .catch(function(err) {
+    .catch(function (err) {
       console.log(err);
     });
 }
 
-window.addEventListener('beforeinstallprompt', function(event) {
+window.addEventListener('beforeinstallprompt', function (event) {
   console.log('beforeinstallprompt fired');
   event.preventDefault();
   deferredPrompt = event;
   return false;
 });
+
+if ('Notification' in window) {
+  function askForNotificationPermission() {
+    Notification.requestPermission(permission => {
+      console.log('User choice:', permission);
+      if (permission !== 'granted') {
+        console.log('No notification permission granted!');
+      } else {
+        console.log('Notification permission granted!');
+      }
+    });
+  }
+
+  const notificationButtons = document.querySelectorAll('.enable-notifications');
+
+  notificationButtons.forEach(button => {
+    button.style.display = 'inline-block';
+
+    button.addEventListener('click', askForNotificationPermission);
+  });
+}
+
