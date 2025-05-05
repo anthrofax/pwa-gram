7b463da287861a13dba8eefaa4edbfc2eb17d09e
anthrofax <afridhoikhsan@gmail.com>
2025-05-05 14:11:51 +0700

    169. Creating our own Polyfills

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -15,7 +15,23 @@ const imagePicker = document.querySelector('#image-picker');
 const iamgePickArea = document.querySelector('#pick-image');
 
 function initializeMedia() {
+  if (!('mediaDevices' in navigator)) {
+    navigator.mediaDevices = {};
+  }
 
+  if (!('getUserMedia' in navigator.mediaDevices)) {
+    navigator.mediaDevices.getUserMedia = function (constraints) {
+      var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+      if (!getUserMedia) {
+        return Promise.reject(new Error('getUserMedia tidak didukung di browser ini'));
+      }
+      
+      return new Promise(function (resolve, reject) {
+        getUserMedia.call(navigator, constraints, resolve, reject);
+      });
+    };
+  }
 }
 
 function openCreatePostModal() {
